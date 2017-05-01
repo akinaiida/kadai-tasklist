8d2addc9dabec12f5fe5585b2b1c374c9a72dda1
aida <ex@gmail.com>
2017-05-01 05:37:08 +0000

    add CRUD and validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,11 +3,16 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
+
+    @include('commons.error_messages')
     
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
         {!! Form::label('content', 'タスク：') !!}
         {!! Form::text('content') !!}
+
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::select('status', ['処理前' => '処理前', '処理中' => '処理中', '処理済' => '処理済']) !!}
         
         {!! Form::submit('登録') !!}
         
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,10 +4,15 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     
+    @include('commons.error_messages')
+    
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
     
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
+        
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::select('status', ['処理前' => '処理前', '処理中' => '処理中', '処理済' => '処理済']) !!}
         
         {!! Form::submit('更新') !!}
         
