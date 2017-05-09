91c600e3bed6df79f8c85fd04f641e9552cfd24d
aida <ex@gmail.com>
2017-05-09 09:10:25 +0000

    destroy add

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -21,4 +21,8 @@
 
     {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id], ['class' => 'btn btn-info']) !!}
 
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    
 @endsection
